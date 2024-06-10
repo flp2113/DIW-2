dce36bc709e11ef654f79be03d4168e02bd55b8e
Felipe Maia <felipe.maia2113@gmail.com>
2024-06-09 21:21:02 -0300

    refactoring code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,24 +2,51 @@ const github_api_url = "https://api.github.com/users/flp2113";
 const github_api_repos_url = "https://api.github.com/users/flp2113/repos";
 const github_api_following_url = "https://api.github.com/users/flp2113/following";
 
-const create_profile = async() => {
-    const profile_section = document.querySelector("#profile");
-    try{
+//PROFILE SECTION
+const profile_section = document.querySelector("#profile");
+
+//REPOSITORIES SECTION
+const repositories_title = document.querySelector("#repositories-title");
+const repositories_section = document.querySelector("#repositories");
+
+//GET REQUEST FUNCTIONS
+const get_user_info = async () => {
+    try {
         const response = await axios.get(github_api_url);
-        const user_profile = `       
-            <img id="profile-picture" src="${response.data.avatar_url}" alt="user profile">
+        return response.data;
+    } catch (error) {
+        profile_section.innerHTML = '<p class="error">Error fetching user information.</p>'
+        console.log("Error fetching user information.", error);
+    }
+}
+
+const get_repositories = async () => {
+    try {
+        const response = await axios.get(github_api_repos_url);
+        return response.data;
+    } catch (error) {
+        repositories_section.innerHTML = '<p class="error">Error fetching user repositories.</p>'
+        console.log("Error fetching user repositories.", error);
+    }
+}
+
+//CREATE FUNCTIONS
+const create_profile = async () => {
+    const data = await get_user_info();
+    const user_profile = `       
+            <img id="profile-picture" src="${data.avatar_url}" alt="user profile">
             <div id="profile-content">
                 <p class="important" id="profile-name">
-                    ${response.data.name}
+                    ${data.name}
                     <span id="profile-followers">
                         <svg class="followers-icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
                         </svg>
-                        ${response.data.followers}
+                        ${data.followers}
                     </span>
                 </p>
-                <p id="profile-bio">${response.data.bio}</p>
-                <p id="profile-location-website"><strong>Location: </strong>${response.data.location}</p>
+                <p id="profile-bio">${data.bio}</p>
+                <p id="profile-location-website"><strong>Location: </strong>${data.location}</p>
                 <div id="profile-icons">
                     <svg class="socialmedia-icons" id="github-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
                         <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
@@ -38,68 +65,51 @@ const create_profile = async() => {
                     </svg>
                 </div>
         `
-        profile_section.innerHTML = user_profile;
-    } catch(error){
-        profile_section.innerHTML = '<p class="error">Error fetching user profile information.</p>'
-        console.log("Error fetching profile information.", error);
-    }
+    profile_section.innerHTML = user_profile;
 }
 
-//FINISH THIS
-const create_repositories = async() => {
-    const repositories_title = document.querySelector("#repositories-title");
-    const repositories_section = document.querySelector("#repositories");
-    try{
-        const response_repos = await axios.get(github_api_repos_url);
-        repositories_title.innerText = `Repositories (${response_repos.data.length})`;
-        for(let i = 0; i < response_repos.data.length; i++){ 
-            try{
-                let response_inner_repos = await axios.get(`https://api.github.com/repos/flp2113/${response_repos.data[i].name}/contents`);
-                let new_card = document.createElement("div");
-                new_card.classList.add("card");
-                /*${response_inner_repos.data.download_url}*/
-                new_card.innerHTML = ` 
+const create_repositories = async () => {
+    const data = await get_repositories();
+    repositories_title.innerText = `Repositories (${data.length})`;
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        let new_card = document.createElement("div");
+        new_card.classList.add("card");
+        new_card.innerHTML = ` 
                     <img src="assets/img/algorithms.png" class="card-img-top" alt="repositoryimage">
                     <div class="card-body">
-                        <h5 class="card-title">${response_repos.data[i].name}</h5>
-                        <p class="card-text">${response_repos.data[i].description}</p>
+                        <h5 class="card-title">${data[i].name}</h5>
+                        <p class="card-text">${data[i].description}</p>
                         <div class="repository-info">
                             <span class="repository-stars">
                                 <svg class="stars-icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                                     <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
                                 </svg>
-                                ${response_repos.data[i].stargazers_count}
+                                ${data[i].stargazers_count}
                             </span>
                             <span class="repository-watchers">
                                 <svg class="followers-icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                     <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
                                 </svg> 
-                                ${response_repos.data[i].watchers}
+                                ${data[i].watchers}
                             </span>
                         </div>
                     </div>
                 `
-                repositories_section.append(new_card);
-            } catch(error){
-                repositories_section.innerHTML = '<p class="error">Error fetching inner user repositories.</p>'
-                console.log("Error fetching inner user repositories.", error);
-            }
-        }
-    } catch(error){
-        repositories_section.innerHTML = '<p class="error">Error fetching user repositories.</p>'
-        console.log("Error fetching user repositories.", error);
+        repositories_section.append(new_card);
     }
 }
 
-const create_team = async() => {
+
+const create_team = async () => {
     const team_section = document.querySelector("#team");
-    try{
+    try {
         const response_team = await axios.get(github_api_following_url);
-        for(let i = 0; i < response_team.data.length; i++){
-            try{
+        for (let i = 0; i < response_team.data.length; i++) {
+            try {
                 const response_inner_team = await axios.get(`https://api.github.com/users/${response_team.data[i].login}`);
                 let team_name = response_inner_team.data.name;
-                if(team_name == null){
+                if (team_name == null) {
                     team_name = response_team.data[i].login;
                 }
                 let new_cell = document.createElement("div");
@@ -113,18 +123,18 @@ const create_team = async() => {
                 </div>
                 `
                 team_section.append(new_cell);
-            } catch(error){
+            } catch (error) {
                 team_section.innerHTML = '<p class="error">Error fetching inner teammates.</p>'
                 console.log("Error fetching inner teammates.", error);
             }
         }
-    } catch(error){
+    } catch (error) {
         team_section.innerHTML = '<p class="error">Error fetching teammates.</p>'
         console.log("Error fetching teammates.", error);
     }
 }
 
 create_profile();
-//create_repositories();
+create_repositories();
 
 //create_team();
